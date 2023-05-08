5504d911d169f060831506ee3dd4ec28f6b491ec
Zed Amaar <mcverdict@gmail.com>
2023-05-08 02:47:08 +0100

    create function templates and simple styling for the main game

--- src/components/snake/SnakeGame.js
+++ src/components/snake/SnakeGame.js
@@ -11,8 +11,64 @@ import {
 } from '../../constants';
 
 const SnakeGame = () => {
+
+  const canvasRef = useRef(null);
+  const [snake, setSnake] = useState(SNAKE_START);
+  const [food, setFood] = useState(FOOD_START);
+  const [dir, setDir] = useState([0,-1]);
+  const [speed, setSpeed] = useState(null);
+  const [gameOver, setGameover] = useState(false);
+
+  const startGame = () => {
+
+  }
+
+  const endGame = () => {
+
+  }
+  const moveSnake = () => {
+    
+  }
+  
+  const generateFood = () => {
+
+  }
+
+  const checkCollision = () => {
+
+  }
+  const checkAppleCollision = () => {
+
+  }
+  
+  const gameLoop = () => {
+
+  }
+
+  useEffect(() => {
+
+  }, [snake, food, gameOver])
+
   return (
-    <div className='snakegame'>Snake Game</div>
+    <div className='snakegamecontainer'>
+      <div className="title">
+      Snake Game
+      </div>
+      <div role='button' tabIndex='0'className="canvaswrapper" onKeyDown={ e=> moveSnake(e)}>
+        <canvas
+         className="canvas"
+         ref={canvasRef}
+         width={`${CANVAS_SIZE[0]}px`}
+         height={`${CANVAS_SIZE[1]}px`}
+        />
+        {gameOver && <div className="gameover">GAME OVER!</div> }
+        <button className="startgame" onClick={startGame}>
+        Start Game
+        </button>
+
+      </div>
+
+    </div>
   )
 }
 
